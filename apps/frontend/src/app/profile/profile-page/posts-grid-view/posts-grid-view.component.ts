7a5a5be1b1e8ba60229972b2ae1b139b871ba268
lewis <lewistham9x@gmail.com>
2021-05-17 15:51:27 +0800

    Fixed baseurl

--- apps/frontend/src/app/profile/profile-page/posts-grid-view/posts-grid-view.component.ts
+++ apps/frontend/src/app/profile/profile-page/posts-grid-view/posts-grid-view.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { environment } from '../../../../environments/environment';
 @Component({
-  selector: 'ia-posts-grid-view',
-  templateUrl: './posts-grid-view.component.html',
-  styleUrls: ['./posts-grid-view.component.scss']
+	selector: 'ia-posts-grid-view',
+	templateUrl: './posts-grid-view.component.html',
+	styleUrls: ['./posts-grid-view.component.scss']
 })
 export class PostsGridViewComponent implements OnInit {
-  @Input() posts;
-  constructor() { }
+	@Input() posts;
+	constructor() {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
