f305f90aa644959285559918af76454b2b864021
lewis <lewistham9x@gmail.com>
2021-05-25 09:37:05 +0800

    Removed ngx-tweet

--- apps/frontend/src/app/profile/profile-page/posts-grid-view/posts-grid-view.component.ts
+++ apps/frontend/src/app/profile/profile-page/posts-grid-view/posts-grid-view.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+
+declare var twttr: any;
 @Component({
 	selector: 'ia-posts-grid-view',
 	templateUrl: './posts-grid-view.component.html',
 	styleUrls: ['./posts-grid-view.component.scss']
 })
-declare var twttr: any;
 export class PostsGridViewComponent implements OnInit {
 	@Input() posts;
 	constructor() {}
